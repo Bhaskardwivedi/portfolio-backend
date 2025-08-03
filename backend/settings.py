b6ccf0919b51927195062dd84388bd9a8a78dbec
Bhaskardwivedi <bhaskardwivedi544@gmail.com>
2025-08-04 01:52:06 +0530

    fix: enable Railway PostgreSQL

--- backend/settings.py
+++ backend/settings.py
@@ -115,10 +115,12 @@ CSRF_TRUSTED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.getenv("RAILWAY") == "TRUE":
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
+}
 
 
 # Password validation
