999df1a119d5fa6c93c1a6373319006a7cf4391e
Bhaskardwivedi <bhaskardwivedi544@gmail.com>
2025-08-08 20:09:05 +0530

    skill category part updated

--- portfolio/aboutus/apps.py
+++ portfolio/aboutus/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AboutusConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio.aboutus'
+    verbose_name = "About Us"
