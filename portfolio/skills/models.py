b481f1c3f95dea14fd57122567e757793cf04da3
Bhaskardwivedi <bhaskardwivedi544@gmail.com>
2025-08-08 19:36:53 +0530

    skill category part updated

--- portfolio/skills/models.py
+++ portfolio/skills/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class SkillCategory(models.Model): 
-    name = models.CharField(max_length=100) 
+    name = models.CharField(max_length=100)
+    category_icon = CloudinaryField('category_icon', blank=True, null=True)
 
     def __str__(self): 
         return self.name
--- portfolio/skills/serializers.py
+++ portfolio/skills/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers 
 from .models import Skill, SkillCategory
 
-class SkillCategorySerializer(serializers.ModelSerializer):
+class SkillCategorySerializer(serializers.ModelSerializer): 
+    category_icon = serializers.SerializerMethodField()
     class Meta:
         model = SkillCategory
-        fields = ['id', 'name']  # Only return ID and Name
+        fields = ['id', 'name', 'category_icon'] 
+
+    def get_category_icon(self, obj):
+        request = self.context.get('request')
+        if obj.category_icon and hasattr(obj.category_icon, 'url'):
+            return request.build_absolute_uri(obj.category_icon.url) if request else obj.category_icon.url
+        return None
+
 
 class SkillSerializer(serializers.ModelSerializer):
     category = SkillCategorySerializer()
